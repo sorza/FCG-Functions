26cc9c5874aa4ac592fbc3bdd8df72b26bb07475
sorza <alexandre.sorza@outlook.com>
2026-01-19 21:13:12 -0300

    refact

--- Program.cs
+++ Program.cs
@@ -1,18 +1,17 @@
 using Microsoft.Azure.Functions.Worker;
-using Microsoft.Azure.Functions.Worker.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using SendGrid;
 
-var builder = FunctionsApplication.CreateBuilder(args);
+var host = new HostBuilder()
+    .ConfigureFunctionsWebApplication()
+    .ConfigureServices(services =>
+    {
+        var sendGridApiKey = Environment.GetEnvironmentVariable("SendGridApiKey") 
+            ?? throw new InvalidOperationException("SendGridApiKey environment variable is not configured.");
+        
+        services.AddSingleton<ISendGridClient>(new SendGridClient(sendGridApiKey));
+    })
+    .Build();
 
-builder.ConfigureFunctionsWebApplication();
-
-builder.Services
-    .AddApplicationInsightsTelemetryWorkerService()
-    .ConfigureFunctionsApplicationInsights();
-
-var sendGridApiKey = Environment.GetEnvironmentVariable("SendGridApiKey");
-builder.Services.AddSingleton<ISendGridClient>(new SendGridClient(sendGridApiKey));
-
-builder.Build().Run();
+host.Run();
